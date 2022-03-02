5d329e71d079d2c992dde5df583f30c6b6e05aec
marcellinus12 <marcellinus.cg@gmail.com>
2022-03-02 22:10:00 +0700

    escape

--- coba.php
+++ coba.php
@@ -13,9 +13,36 @@
 
         // echo $a + $b;
 
-        $array = array("Satu", "Dua", "Tiga");
-        $array2 = array(1, 2, 3);
-        echo $array[1]."<br>".$array2[1];
+        // $array = array("Satu", "Dua", "Tiga");
+        // $array2 = array(1, 2, 3);
+        // echo $array[1]."<br>".$array2[1];
+
+        $input = 40;
+
+        if ($input>=80 && $input<=100) {
+            echo "Nilai anda A";
+        } else if($input>=60 && $input<=79){
+            echo "Nilai anda B";
+        } else if($input>0 && $input<=59){
+            echo "Nilai anda C";
+        } else{
+            echo "Nilai anda belum masuk";
+        }
+        
+        echo "<br>";
+
+        $mesin = "Bagus";
+        $body = "Bagus";
+
+        if ($mesin=="Bagus" && $body=="Bagus") {
+            echo "Bagus";
+        } else if($mesin=="Bagus" || $body=="Bagus"){
+            echo "Menengah";
+        } else if($mesin=="Jelek" && $body=="Jelek"){
+            echo "Jelek";
+        }
+        
+
     ?>
 </body>
 </html>
