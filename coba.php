cbeaae2836d34326bae15f9a7ffc46652c10959e
marcellinus12 <marcellinus.cg@gmail.com>
2022-03-02 21:23:59 +0700

    ade2

---
+++ coba.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // $a = 1;
+        // $b = 10;
+
+        // echo $a + $b;
+
+        $array = array("Satu", "Dua", "Tiga");
+        $array2 = array(1, 2, 3);
+        echo $array[1]."<br>".$array2[1];
+    ?>
+</body>
+</html>
